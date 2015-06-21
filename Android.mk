a164ed560c00904214b215262498fa33384643c1
cbolumar <cbolumar@yahoo.es>
2015-06-21 15:51:31 +0200

    Remove all
    Change-Id: Idee21e179802e1a5691242373d3c181e10b817f7

--- Android.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(TARGET_DEVICE),codinap)
-    LOCAL_PATH := $(call my-dir)
-    include $(call first-makefiles-under,$(LOCAL_PATH))
-endif
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo cm_codinap-eng
-add_lunch_combo cm_codinap-userdebug
