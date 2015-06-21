ad5d043ac9be26d982e60c9a588ea1c24d4b647f
cbolumar <cbolumar@yahoo.es>
2015-06-21 15:05:48 +0200

    Remove all
    Change-Id: Ic70b10bfcb276a136856532184c10b4ad257cbc9

--- Android.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(TARGET_DEVICE),codina)
-    LOCAL_PATH := $(call my-dir)
-    include $(call first-makefiles-under,$(LOCAL_PATH))
-endif
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo cm_codina-user
-add_lunch_combo cm_codina-userdebug
-
